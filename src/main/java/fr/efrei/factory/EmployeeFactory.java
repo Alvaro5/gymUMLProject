7ec776294ae8cf01215416b3d499888950135200
Anne-Laure <anne-laure.parguet@efrei.net>
2024-11-26 14:08:33 +0100

    Update EmployeeFactory and Coach classes

--- src/main/java/fr/efrei/factory/EmployeeFactory.java
+++ src/main/java/fr/efrei/factory/EmployeeFactory.java
@@ -4,30 +4,63 @@ import fr.efrei.domain.Employee;
 import fr.efrei.util.Helper;
 
 public class EmployeeFactory {
-    public static Employee buildEmployee (int id, String firstName, String lastName, boolean onRestDAy, String bankDetails){
-        if(Helper.isNullOrEmpty(String.valueOf(id)) || Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(firstName)){
+
+
+
+    public static Employee buildEmployee (int employeeId, String firstName, String lastName, boolean onRestDAy, String bankDetails){
+        if(Helper.isNullOrEmpty(String.valueOf(employeeId))){
+            System.out.println("id is incorrect");
+            return null;
+        }
+        if (Helper.isNullOrEmpty(lastName)){
+            System.out.println("last name is incorrect");
+            return null;
+        }
+        if (Helper.isNullOrEmpty(firstName)){
+            System.out.println("first name is incorrect");
+            return null;
+        }
+        if (Helper.isNullOrEmpty(String.valueOf(onRestDAy))){
+            System.out.println("rest day is incorrect");
+            return null;
+        }
+        if (Helper.isNullOrEmpty(bankDetails)){
+            System.out.println("bank details are incorrect");
             return null;
         }
 
-        return new Employee.EmployeeBuilder().setId(id)
+        return new Employee.EmployeeBuilder().setEmployeeId(employeeId)
                 .setLastName(lastName)
                 .setFirstName(firstName)
                 .setOnRestDay(onRestDAy)
-                .setBank_details(bankDetails)
+                .setBankDetails(bankDetails)
                 .build();
     }
 
     public static Employee buildEmployee (String firstName, String lastName, boolean onRestDAy, String bankDetails){
-        if(Helper.isNullOrEmpty(lastName) || Helper.isNullOrEmpty(firstName)){
+        if (Helper.isNullOrEmpty(lastName)){
+            System.out.println("last name is incorrect");
+            return null;
+        }
+        if (Helper.isNullOrEmpty(firstName)){
+            System.out.println("first name is incorrect");
+            return null;
+        }
+        if (Helper.isNullOrEmpty(String.valueOf(onRestDAy))){
+            System.out.println("rest day is incorrect");
+            return null;
+        }
+        if (Helper.isNullOrEmpty(bankDetails)){
+            System.out.println("bank details are incorrect");
             return null;
         }
 
-        int id = Helper.generateId();
-        return new Employee.EmployeeBuilder().setId(id)
+        int employeeId = Helper.generateId();
+        return new Employee.EmployeeBuilder().setEmployeeId(employeeId)
                 .setLastName(lastName)
                 .setFirstName(firstName)
                 .setOnRestDay(onRestDAy)
-                .setBank_details(bankDetails)
+                .setBankDetails(bankDetails)
                 .build();
     }
 
